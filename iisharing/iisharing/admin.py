65f74d4c3f82b266c44bec6444b17712d155edad
Ed Crewe <cmetc@FatLapCrewe.(none)>
2011-07-09 13:46:10 +0100

    add other items to admin

--- iisharing/iisharing/admin.py
+++ iisharing/iisharing/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 
 from iisharing.models import Organization
+from iisharing.models import Item
+from iisharing.models import UnitOfMeasure
 
 admin.site.register(Organization)
+admin.site.register(UnitOfMeasure)
+admin.site.register(Item)
