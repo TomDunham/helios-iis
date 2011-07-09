1044f37394c6f70da2bbb27cc0a3d0be368f46d0
Tom Dunham <tom@tomdunham.org>
2011-07-09 15:22:20 +0100

    Really basic filters

--- iisharing/iisharing/filters.py
+++ iisharing/iisharing/filters.py
@@ -2,7 +2,7 @@ import django_filters
 
 from iisharing.models import Item
 
-class ProductFilterSet(django_filters.FilterSet):
+class StockFilterSet(django_filters.FilterSet):
     class Meta:
         model = Item
         fields = ['shared_code', 'organization']
--- iisharing/iisharing/views.py
+++ iisharing/iisharing/views.py
@@ -1,6 +1,13 @@
 from django.views.generic import ListView
 
 from iisharing.models import Item
+from iisharing.filters import StockFilterSet
 
 class StockView(ListView):
     model = Item
+
+    def get_context_data(self, **kwargs):
+        ctx = super(StockView, self).get_context_data(**kwargs)
+        ctx.update(filter = StockFilterSet(self.request.GET or None))
+        return ctx
+
