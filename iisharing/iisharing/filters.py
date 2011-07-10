e23ad6740a3a2d534d568da22c3f6fb92b0da1be
Tom Dunham <tom@tomdunham.org>
2011-07-10 13:15:06 +0100

    Allows selection of many countries / orgs / etc

--- iisharing/iisharing/filters.py
+++ iisharing/iisharing/filters.py
@@ -1,8 +1,18 @@
 import django_filters
 
-from iisharing.models import Item
+from iisharing.models import Item, Country, Organization, STATUS_CHOICES
 
 class StockFilterSet(django_filters.FilterSet):
+    country = django_filters.ModelMultipleChoiceFilter(
+        queryset=Country.objects.all())
+
+    organization = django_filters.ModelMultipleChoiceFilter(
+        queryset=Organization.objects.all())
+
+    status = django_filters.MultipleChoiceFilter(
+        choices = STATUS_CHOICES)
+
+
     class Meta:
         model = Item
-        fields = ['country', 'shared_code', 'organization']
+        fields = ['country', 'shared_code', 'organization', 'status']
