6b5d8e2640865927ef748c60470d8d4de9472c40
Tom Dunham <tom@tomdunham.org>
2011-07-10 15:52:16 +0100

    Pagination

--- iisharing/iisharing/views.py
+++ iisharing/iisharing/views.py
@@ -5,9 +5,16 @@ from iisharing.filters import StockFilterSet
 
 class StockView(ListView):
     model = Item
+    paginate_by = 100
+
+    def get_filterset(self):
+        return StockFilterSet(self.request.GET or None)
+
+    def get_queryset(self):
+        return self.get_filterset().qs
 
     def get_context_data(self, **kwargs):
         ctx = super(StockView, self).get_context_data(**kwargs)
-        ctx.update(filter = StockFilterSet(self.request.GET or None))
+        ctx.update(form = self.get_filterset().form)
         return ctx
 
